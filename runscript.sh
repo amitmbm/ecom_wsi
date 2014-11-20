9e9a3a8efb9408b64145100ea1884cec6509937a
rohit.laddha <rohit.laddha@kony.com>
2014-11-20 22:43:43 +0530

    testing

--- runscript.sh
+++ runscript.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mvn clean install -DskipTests=true
-rm -rf /d/saas/SaaSFoundation/public/apache-tomcat-7.0.37/webapps/olx*
-cp target/olx.war  /d/saas/SaaSFoundation/public/apache-tomcat-7.0.37/webapps/
+rm -rf C:/tomcat7/apache-tomcat-7.0.54/webapps/olx*
+cp target/olx.war C:/tomcat7/apache-tomcat-7.0.54/webapps/
