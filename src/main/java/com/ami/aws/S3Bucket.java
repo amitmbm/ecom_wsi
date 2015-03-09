cc4d0184a28485814e64903be439c486a4948e2e
amit.khandelwal <amit.khandelwal@kony.com>
2015-03-09 11:47:09 +0530

    commenting the S3 bucket access code

--- src/main/java/com/ami/aws/S3Bucket.java
+++ src/main/java/com/ami/aws/S3Bucket.java
@@ -1,29 +1,26 @@
 package com.ami.aws;
 
-import java.util.List;
-
 import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Component;
 
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.Bucket;
+/*
+ * commenting as it is threat to AWS account , as well deleting.
+ */
 
 @Component
 public class S3Bucket {
 
 	private static final String bucketName     = "olxlogs";
 	
-	private static final String Aws_Access_Key = System.getProperty("AWS_KEY");
+/*	private static final String Aws_Access_Key = System.getProperty("AWS_KEY");
 	private static final String Aws_Secret_Key = System
-			.getProperty("AWS_SECRET");
+			.getProperty("AWS_SECRET");*/
 
 	@PostConstruct
 	public void printS3Bucket()
 	{
-		System.out.println("printing key" + Aws_Access_Key);
+		/*System.out.println("printing key" + Aws_Access_Key);
 		System.out.println("printing key" + Aws_Secret_Key);
 		System.out.println("printing key" + Aws_Access_Key);
 		System.out.println("printing key" + Aws_Secret_Key);
@@ -31,26 +28,8 @@ public class S3Bucket {
 	               Aws_Access_Key, Aws_Secret_Key));
 		
 		List<Bucket> buck = s3.listBuckets();
-		System.out.println("printing the bucket in AWS" + buck);
+		System.out.println("printing the bucket in AWS" + buck);*/
 	}
-/*static
-{
-	System.out.println("printing key" + Aws_Access_Key);
-	System.out.println("printing key" + Aws_Secret_Key);
-}
-	// public static void main(String[] args) throws IOException {
-	// Instantiate the S3 client with your AWS credentials
-	@Test
-	public void test() {
 
-		System.out.println("printing key" + Aws_Access_Key);
-		System.out.println("printing key" + Aws_Secret_Key);
-		AmazonS3 s3 = new AmazonS3Client(new BasicAWSCredentials(
-	               Aws_Access_Key, Aws_Secret_Key));
-		
-		List<Bucket> buck = s3.listBuckets();
-		System.out.println("printing the bucket in AWS" + buck);
-
-}*/
 
 }
