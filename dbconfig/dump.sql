14c38987553a12dae7ec52a3f264ea2245b514a1
satpal <satpaljaggi@gmail.com>
2015-05-30 00:16:09 +0530

    added acls table

---
+++ dbconfig/dump.sql
@@ -0,0 +1,344 @@
+-- MySQL dump 10.13  Distrib 5.6.23, for Win32 (x86)
+--
+-- Host: localhost    Database: wsi_db
+-- ------------------------------------------------------
+-- Server version	5.6.23-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `acls`
+--
+
+DROP TABLE IF EXISTS `acls`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `acls` (
+  `id` varchar(36) NOT NULL,
+  `grp_name` varchar(36) NOT NULL,
+  `res_name` varchar(128) NOT NULL,
+  `perm_name` varchar(128) NOT NULL,
+  `created_by` varchar(128) DEFAULT NULL,
+  `updated_by` varchar(128) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `grp_fk1` (`grp_name`),
+  KEY `res_fk1` (`res_name`),
+  KEY `perm_fk1` (`perm_name`),
+  CONSTRAINT `grp_fk1` FOREIGN KEY (`grp_name`) REFERENCES `group` (`name`),
+  CONSTRAINT `perm_fk1` FOREIGN KEY (`perm_name`) REFERENCES `permission` (`name`),
+  CONSTRAINT `res_fk1` FOREIGN KEY (`res_name`) REFERENCES `resource` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `acls`
+--
+
+LOCK TABLES `acls` WRITE;
+/*!40000 ALTER TABLE `acls` DISABLE KEYS */;
+/*!40000 ALTER TABLE `acls` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `group`
+--
+
+DROP TABLE IF EXISTS `group`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `group` (
+  `id` varchar(36) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `type` varchar(128) NOT NULL,
+  `created_by` varchar(128) DEFAULT NULL,
+  `updated_by` varchar(128) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `group`
+--
+
+LOCK TABLES `group` WRITE;
+/*!40000 ALTER TABLE `group` DISABLE KEYS */;
+/*!40000 ALTER TABLE `group` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `permission`
+--
+
+DROP TABLE IF EXISTS `permission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `permission` (
+  `id` varchar(36) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `created_by` varchar(128) DEFAULT NULL,
+  `updated_by` varchar(128) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permission`
+--
+
+LOCK TABLES `permission` WRITE;
+/*!40000 ALTER TABLE `permission` DISABLE KEYS */;
+/*!40000 ALTER TABLE `permission` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `post_add`
+--
+
+DROP TABLE IF EXISTS `post_add`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `post_add` (
+  `add_guid` varchar(36) NOT NULL,
+  `sub_cat_guid` varchar(36) NOT NULL,
+  `type_guid` varchar(36) NOT NULL,
+  `add_desc` varchar(2048) NOT NULL,
+  `price` int(11) NOT NULL,
+  `negotiable` tinyint(1) DEFAULT '0',
+  `user_email` varchar(128) NOT NULL,
+  `image_1` mediumtext,
+  `image_2` mediumtext,
+  `image_3` mediumtext,
+  `image_4` mediumtext,
+  `created_by` varchar(128) DEFAULT NULL,
+  `updated_by` varchar(128) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`add_guid`),
+  KEY `post_add_fk1` (`sub_cat_guid`),
+  KEY `post_add_fk2` (`type_guid`),
+  KEY `post_add_fk3` (`user_email`),
+  KEY `post_add_price_index` (`price`),
+  CONSTRAINT `post_add_fk1` FOREIGN KEY (`sub_cat_guid`) REFERENCES `product_sub_category` (`sub_cat_guid`) ON DELETE CASCADE,
+  CONSTRAINT `post_add_fk2` FOREIGN KEY (`type_guid`) REFERENCES `product_sub_category_type` (`type_guid`) ON DELETE CASCADE,
+  CONSTRAINT `post_add_fk3` FOREIGN KEY (`user_email`) REFERENCES `users` (`user_email`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post_add`
+--
+
+LOCK TABLES `post_add` WRITE;
+/*!40000 ALTER TABLE `post_add` DISABLE KEYS */;
+/*!40000 ALTER TABLE `post_add` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_category`
+--
+
+DROP TABLE IF EXISTS `product_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_category` (
+  `cat_guid` varchar(36) NOT NULL,
+  `cat_name` varchar(128) NOT NULL,
+  `cat_desc` varchar(512) NOT NULL,
+  `created_by` varchar(128) DEFAULT NULL,
+  `updated_by` varchar(128) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`cat_guid`),
+  UNIQUE KEY `cat_name` (`cat_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_category`
+--
+
+LOCK TABLES `product_category` WRITE;
+/*!40000 ALTER TABLE `product_category` DISABLE KEYS */;
+INSERT INTO `product_category` VALUES ('d4865262-9ec5-4ba2-9842-ea50be919e5a','first AND second','first category',NULL,NULL,'2015-05-12 19:51:00','2015-05-16 17:54:08');
+/*!40000 ALTER TABLE `product_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_sub_category`
+--
+
+DROP TABLE IF EXISTS `product_sub_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_sub_category` (
+  `sub_cat_guid` varchar(36) NOT NULL,
+  `sub_cat_name` varchar(128) NOT NULL,
+  `sub_cat_desc` varchar(512) NOT NULL,
+  `cat_guid` varchar(36) DEFAULT NULL,
+  `created_by` varchar(128) DEFAULT NULL,
+  `updated_by` varchar(128) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`sub_cat_guid`),
+  UNIQUE KEY `sub_cat_name` (`sub_cat_name`),
+  KEY `product_sub_category_fk1` (`cat_guid`),
+  CONSTRAINT `product_sub_category_fk1` FOREIGN KEY (`cat_guid`) REFERENCES `product_category` (`cat_guid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_sub_category`
+--
+
+LOCK TABLES `product_sub_category` WRITE;
+/*!40000 ALTER TABLE `product_sub_category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_sub_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_sub_category_type`
+--
+
+DROP TABLE IF EXISTS `product_sub_category_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_sub_category_type` (
+  `type_guid` varchar(36) NOT NULL,
+  `sub_cat_guid` varchar(36) NOT NULL,
+  `type_name` varchar(128) NOT NULL,
+  `created_by` varchar(128) DEFAULT NULL,
+  `updated_by` varchar(128) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`type_guid`),
+  UNIQUE KEY `sub_cat_type_unique` (`sub_cat_guid`,`type_name`),
+  CONSTRAINT `product_sub_category_type_fk1` FOREIGN KEY (`sub_cat_guid`) REFERENCES `product_sub_category` (`sub_cat_guid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_sub_category_type`
+--
+
+LOCK TABLES `product_sub_category_type` WRITE;
+/*!40000 ALTER TABLE `product_sub_category_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_sub_category_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `resource`
+--
+
+DROP TABLE IF EXISTS `resource`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `resource` (
+  `id` varchar(36) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `created_by` varchar(128) DEFAULT NULL,
+  `updated_by` varchar(128) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `resource`
+--
+
+LOCK TABLES `resource` WRITE;
+/*!40000 ALTER TABLE `resource` DISABLE KEYS */;
+/*!40000 ALTER TABLE `resource` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_profile`
+--
+
+DROP TABLE IF EXISTS `user_profile`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_profile` (
+  `first_name` varchar(128) NOT NULL,
+  `last_name` varchar(128) NOT NULL,
+  `phone_num` bigint(20) DEFAULT NULL,
+  `user_email` varchar(128) NOT NULL,
+  `created_by` varchar(128) DEFAULT NULL,
+  `updated_by` varchar(128) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_email`),
+  CONSTRAINT `user_profile_fk1` FOREIGN KEY (`user_email`) REFERENCES `users` (`user_email`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_profile`
+--
+
+LOCK TABLES `user_profile` WRITE;
+/*!40000 ALTER TABLE `user_profile` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_profile` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user_email` varchar(128) NOT NULL,
+  `user_passwd` varchar(128) NOT NULL,
+  `is_registerd` tinyint(1) DEFAULT '0',
+  `user_key` varchar(36) DEFAULT NULL,
+  `grp_name` varchar(128) DEFAULT NULL,
+  `created_by` varchar(128) DEFAULT NULL,
+  `updated_by` varchar(128) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_email`),
+  KEY `user_grp_fk1` (`grp_name`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`grp_name`) REFERENCES `group` (`name`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-05-30  0:14:29
