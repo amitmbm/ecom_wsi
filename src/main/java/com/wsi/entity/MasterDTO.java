14eb58d9dbc0c51f6ed962b0ab08b3679139ec12
satpal <satpaljaggi@gmail.com>
2015-06-04 20:41:10 +0530

    modifying grooups for timestamp related changes, updatedat / createat will take value directly from database instead of passing from java code, added masterdto

---
+++ src/main/java/com/wsi/entity/MasterDTO.java
@@ -0,0 +1,44 @@
+package com.wsi.entity;
+
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class MasterDTO {
+	protected String createdBy;
+	protected String updatedBy;
+	protected Date createdAt;
+	protected Date updatedAt;
+	
+	
+	@XmlElement(name = "updated_by")
+	public String getUpdatedBy() {
+		return this.updatedBy;
+	}
+
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	@XmlElement(name = "created_at")
+	public Date getCreatedAt() {
+		return this.createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	@XmlElement(name = "updated_at")
+	public Date getUpdatedAt() {
+		return this.updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+
+}
